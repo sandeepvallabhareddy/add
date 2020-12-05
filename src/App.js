f38870c6aaf5e8e1e967df2463be90bbecf2850d
sandeepvallabhareddy <sandeepvallabhareddy@gmail.com>
2020-12-06 00:49:37 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from 'react'
 import OrganizationProfile from './components/organization_profile/organization_profile'
 import SignUp from './components/signup/signup'
 import Login from './components/signup/login'
-import {BrowserRouter,Route,Link,Switch} from 'react-router-dom'
+import {HashRouter,Route,Link,Switch} from 'react-router-dom'
 import UserProfile from './components/user_profile/user_profile'
 function App()
 {
   return(
   	<div className="app">
+		  <h1>sandeep</h1>
+  		<HashRouter>
 		  <OrganizationProfile/>
-  		<BrowserRouter>
   			<Route exact path="/signup" component={SignUp}/>
 			  <Route exact path="/login" component={Login}/>
   			<Route exact path="/userprofile" component={UserProfile}/>
 			  <Route exact path="/organizationprofile" component={OrganizationProfile}/>
-  		</BrowserRouter>
+  		</HashRouter>
   	</div>
   )
 }
