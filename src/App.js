2d388cbb8ec103f5d62e1c418c08cbcd11d93e76
sandeepvallabhareddy <sandeepvallabhareddy@gmail.com>
2020-12-06 00:29:13 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App()
 {
   return(
   	<div className="app">
+		  <OrganizationProfile/>
   		<BrowserRouter>
   			<Route exact path="/signup" component={SignUp}/>
 			  <Route exact path="/login" component={Login}/>
